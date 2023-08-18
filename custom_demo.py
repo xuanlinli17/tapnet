ce508027e93463e3c97662e29a4a220d854405b4
xuanlinli17 <xuanlinli17@gmail.com>
2023-08-17 17:50:45 -0700

    update

---
+++ custom_demo.py
@@ -0,0 +1,301 @@
+# pip install --upgrade "jax[cuda11_local]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# RUN EVERYTHING UNDER PARENT DIRECTORY OF tapnet: e.g., python tapnet/custom_demo.py
+import functools
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false' # prevents jax from allocating large trunks of memory
+import haiku as hk
+import jax
+import jax.numpy as jnp
+# jax.config.update('jax_platform_name', 'cpu') # cpu is very very slow
+import matplotlib.pyplot as plt
+import mediapy as media
+import numpy as np
+from tqdm import tqdm
+import tree
+
+from tapnet import tapir_model
+from tapnet.utils import transforms
+from tapnet.utils import viz_utils
+
+# gpus = jax.devices('gpu')
+# device = gpus[1] # If you want to use "cuda:1"
+checkpoint_path = 'tapnet/checkpoints/causal_tapir_checkpoint.npy'
+ckpt_state = np.load(checkpoint_path, allow_pickle=True).item()
+params, state = ckpt_state['params'], ckpt_state['state']
+
+# @title Build Model {form-width: "25%"}
+
+# Internally, the tapir model has three stages of processing: computing
+# image features (get_feature_grids), extracting features for each query point
+# (get_query_features), and estimating trajectories given query features and
+# the feature grids where we want to track (estimate_trajectories).  For
+# tracking online, we need extract query features on the first frame only, and
+# then call estimate_trajectories on one frame at a time.
+
+def build_online_model_init(frames, query_points):
+  """Initialize query features for the query points."""
+  model = tapir_model.TAPIR(use_causal_conv=True, bilinear_interp_with_depthwise_conv=False)
+
+  feature_grids = model.get_feature_grids(frames, is_training=False)
+  query_features = model.get_query_features(
+      frames,
+      is_training=False,
+      query_points=query_points,
+      feature_grids=feature_grids,
+  )
+  return query_features
+
+
+def build_online_model_predict(frames, query_features, causal_context):
+  """Compute point tracks and occlusions given frames and query points."""
+  model = tapir_model.TAPIR(use_causal_conv=True, bilinear_interp_with_depthwise_conv=False)
+  feature_grids = model.get_feature_grids(frames, is_training=False)
+  trajectories = model.estimate_trajectories(
+      frames.shape[-3:-1],
+      is_training=False,
+      feature_grids=feature_grids,
+      query_features=query_features,
+      query_points_in_video=None,
+      query_chunk_size=64,
+      causal_context=causal_context,
+      get_causal_context=True,
+  )
+  causal_context = trajectories['causal_context']
+  del trajectories['causal_context']
+  return {k: v[-1] for k, v in trajectories.items()}, causal_context
+
+
+online_init = hk.transform_with_state(build_online_model_init)
+online_init_apply = jax.jit(online_init.apply)
+
+online_predict = hk.transform_with_state(build_online_model_predict)
+online_predict_apply = jax.jit(online_predict.apply)
+
+print("Jit complete")
+
+rng = jax.random.PRNGKey(42)
+online_init_apply = functools.partial(
+    online_init_apply, params=params, state=state, rng=rng
+)
+online_predict_apply = functools.partial(
+    online_predict_apply, params=params, state=state, rng=rng
+)
+
+# @title Utility Functions {form-width: "25%"}
+
+def preprocess_frames(frames):
+  """Preprocess frames to model inputs.
+
+  Args:
+    frames: [num_frames, height, width, 3], [0, 255], np.uint8
+
+  Returns:
+    frames: [num_frames, height, width, 3], [-1, 1], np.float32
+  """
+  frames = frames.astype(np.float32)
+  frames = frames / 255 * 2 - 1
+  return frames
+
+
+def postprocess_occlusions(occlusions, expected_dist):
+  """Postprocess occlusions to boolean visible flag.
+
+  Args:
+    occlusions: [num_points, num_frames], [-inf, inf], np.float32
+
+  Returns:
+    visibles: [num_points, num_frames], bool
+  """
+  pred_occ = jax.nn.sigmoid(occlusions)
+  pred_occ = 1 - (1 - pred_occ) * (1 - jax.nn.sigmoid(expected_dist))
+  visibles = pred_occ < 0.5  # threshold
+  return visibles
+
+
+def sample_random_points(frame_max_idx, height, width, num_points):
+  """Sample random points with (time, height, width) order."""
+  y = np.random.randint(0, height, (num_points, 1))
+  x = np.random.randint(0, width, (num_points, 1))
+  t = np.random.randint(0, frame_max_idx + 1, (num_points, 1))
+  points = np.concatenate((t, y, x), axis=-1).astype(np.int32)  # [num_points, 3]
+  return points
+
+
+def construct_initial_causal_state(num_points, num_resolutions):
+  value_shapes = {
+      "tapir/~/pips_mlp_mixer/block_1_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_1_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_2_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_2_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_3_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_3_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_4_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_4_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_5_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_5_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_6_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_6_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_7_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_7_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_8_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_8_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_9_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_9_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_10_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_10_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_11_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_11_causal_2": (1, num_points, 2, 2048),
+      "tapir/~/pips_mlp_mixer/block_causal_1": (1, num_points, 2, 512),
+      "tapir/~/pips_mlp_mixer/block_causal_2": (1, num_points, 2, 2048),
+  }
+  fake_ret = {
+      k: jnp.zeros(v, dtype=jnp.float32) for k, v in value_shapes.items()
+  }
+  return [fake_ret] * num_resolutions * 4
+
+
+video = media.read_video('/home/xuanlin/Downloads/20230815_205733_cse_2_pink_mugs.mp4')
+height, width = video.shape[1:3]
+print("Load video complete")
+
+# # @title Progressively Predict Sparse Point Tracks {form-width: "25%"}
+
+# resize_height = 256  # @param {type: "integer"}
+# resize_width = 256  # @param {type: "integer"}
+# num_points = 256  # @param {type: "integer"}
+
+# frames = media.resize_video(video, (resize_height, resize_width))
+# query_points = sample_random_points(0, frames.shape[1], frames.shape[2], num_points) # [num_points, 3]
+
+# query_features, _ = online_init_apply(frames=preprocess_frames(frames[None, None, 0]), query_points=query_points[None])
+# causal_state = construct_initial_causal_state(query_points.shape[0], len(query_features.resolutions) - 1)
+
+# # Predict point tracks frame by frame
+# predictions = []
+# for i in range(frames.shape[0]):
+#   print(i)
+#   (prediction, causal_state), _ = online_predict_apply(
+#       frames=preprocess_frames(frames[None, None, i]),
+#       query_features=query_features,
+#       causal_context=causal_state,
+#   )
+#   predictions.append(prediction)
+
+# tracks = np.concatenate([x['tracks'][0] for x in predictions], axis=1)
+# occlusions = np.concatenate([x['occlusion'][0] for x in predictions], axis=1)
+# expected_dist = np.concatenate([x['expected_dist'][0] for x in predictions], axis=1)
+
+# visibles = postprocess_occlusions(occlusions, expected_dist)
+
+# # Visualize sparse point tracks
+# tracks = transforms.convert_grid_coordinates(tracks, (resize_width, resize_height), (width, height))
+# print("creating video")
+# video_viz = viz_utils.paint_point_track(video, tracks, visibles)
+# print("video create complete")
+# media.write_video('/home/xuanlin/Downloads/vis.mp4', video_viz, fps=10)
+
+
+
+# @title Select Any Points at First Frame {form-width: "25%"}
+
+# Generate a colormap with 20 points, no need to change unless select more than 20 points
+colormap = viz_utils.get_colors(20)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.imshow(video[0])
+ax.axis('off')
+ax.set_title('You can select more than 1 points. After select enough points, run the next cell.')
+
+select_points = []
+
+ # Event handler for mouse clicks
+def on_click(event):
+  if event.button == 1 and event.inaxes == ax:  # Left mouse button clicked
+    x, y = int(np.round(event.xdata)), int(np.round(event.ydata))
+
+    select_points.append(np.array([x, y]))
+
+    color = colormap[len(select_points) - 1]
+    color = tuple(np.array(color) / 255.0)
+    ax.plot(x, y, 'o', color=color, markersize=5)
+    plt.draw()
+
+fig.canvas.mpl_connect('button_press_event', on_click)
+plt.show()
+# you need to manually close the plot window here
+print("tracking...")
+# @title Predict Point Tracks for the Selected Points {form-width: "25%"}
+
+resize_height = 256  # @param {type: "integer"}
+resize_width = 256  # @param {type: "integer"}
+
+def convert_select_points_to_query_points(frame, points):
+  """Convert select points to query points.
+
+  Args:
+    points: [num_points, 2], [t, y, x]
+  Returns:
+    query_points: [num_points, 3], [t, y, x]
+  """
+  points = np.stack(points)
+  query_points = np.zeros(shape=(points.shape[0], 3), dtype=np.float32)
+  query_points[:, 0] = frame
+  query_points[:, 1] = points[:, 1]
+  query_points[:, 2] = points[:, 0]
+  return query_points
+
+frames = media.resize_video(video, (resize_height, resize_width))
+print("resize video complete")
+
+import time
+tt = time.time()
+query_points = convert_select_points_to_query_points(0, select_points)
+query_points = transforms.convert_grid_coordinates(
+    query_points, (1, height, width), (1, resize_height, resize_width), coordinate_format='tyx')
+print("convert select points to query points complete", time.time() - tt)
+
+tt = time.time()
+query_features, _ = online_init_apply(frames=preprocess_frames(frames[None, None, -1]), query_points=query_points[None])
+print("initial query features jit complete", time.time() - tt)
+tt = time.time()
+query_features, _ = online_init_apply(frames=preprocess_frames(frames[None, None, 0]), query_points=query_points[None])
+print("initial query features complete", time.time() - tt)
+
+tt = time.time()
+causal_state = construct_initial_causal_state(query_points.shape[0] * 2, len(query_features.resolutions) - 1)
+print("initial causal state jit? complete", time.time() - tt)
+tt = time.time()
+causal_state = construct_initial_causal_state(query_points.shape[0], len(query_features.resolutions) - 1)
+print("initial causal state complete", time.time() - tt)
+
+# dummy iteration for jax to compile
+tt = time.time()
+online_predict_apply(
+      frames=preprocess_frames(frames[None, None, 0]),
+      query_features=query_features,
+      causal_context=causal_state,
+  )
+print("jax compile complete", time.time() - tt)
+# Predict point tracks frame by frame
+predictions = []
+for i in tqdm(range(frames.shape[0])):
+  print(i)
+  (prediction, causal_state), _ = online_predict_apply(
+      frames=preprocess_frames(frames[None, None, i]),
+      query_features=query_features,
+      causal_context=causal_state,
+  )
+  predictions.append(prediction)
+
+tracks = np.concatenate([x['tracks'][0] for x in predictions], axis=1)
+occlusions = np.concatenate([x['occlusion'][0] for x in predictions], axis=1)
+expected_dist = np.concatenate([x['expected_dist'][0] for x in predictions], axis=1)
+
+visibles = postprocess_occlusions(occlusions, expected_dist)
+
+# Visualize sparse point tracks
+tracks = transforms.convert_grid_coordinates(tracks, (resize_width, resize_height), (width, height))
+print("video creating...")
+video_viz = viz_utils.paint_point_track(video, tracks, visibles, colormap)
+print("video create complete")
+media.write_video('/home/xuanlin/Downloads/vis.mp4', video_viz, fps=10)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open("requirements_inference.txt", "r") as f:
+        lines = [l.strip() for l in f.readlines()]
+    install_requires = list(filter(None, lines))
+    return install_requires
+
+if __name__ == "__main__":
+    setup(
+        name='tapnet',
+        version='0.0.1',
+        description="TAPIR DeepMind",
+        install_requires=read_requirements(),
+        packages=find_packages(include=[]),
+    )
